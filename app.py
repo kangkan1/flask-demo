3f1370d8ca3e6c2870380a01d3e2492022c54368
NoobMater69 <kangkanlahkar1@gmail.com>
2021-10-14 16:34:50 +0530

    New screens added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route('/')
@@ -8,10 +10,20 @@ def hello():
     return render_template("index.html")
 
 
+@app.route('/bookings/')
+def bookings():
+    return render_template("bookings.html")  
+
+
 @app.route('/about/')
 def about():
-    return "<h3>This is a Flask web application.</h3>"    
+    return render_template("about.html")   
 
-    
+
+@app.route('/settings/')
+def settings():
+    return render_template("settings.html")
+
 if __name__ == "__main__":
 	hello()
+    
